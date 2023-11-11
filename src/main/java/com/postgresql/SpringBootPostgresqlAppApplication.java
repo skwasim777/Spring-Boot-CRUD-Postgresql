6bd0c4de854526aa4d1d70106fd8660a1f07ade8
skwasim777 <wasimsk6061@gmail.com>
2023-11-11 16:06:18 +0530

    crud operation using postgresql

---
+++ src/main/java/com/postgresql/SpringBootPostgresqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgresqlAppApplication.class, args);
+	}
+
+}
